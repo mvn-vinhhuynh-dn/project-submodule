5f39607564f0cd3e4c68b0abb7f8ade2c8b75843
vinh.huynh <vinh.huynh@asiantech.vn>
2017-03-09 15:36:07 +0700

    init

--- app/src/main/java/coder/victorydst3/com/demosubmodul/MainActivity.java
+++ app/src/main/java/coder/victorydst3/com/demosubmodul/MainActivity.java
@@ -11,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTvTest = (TextView) findViewById(R.id.tvTest);
-
     }
 }
